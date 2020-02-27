f66e26a45dc456c07d1c83491577498734a1b238
Robin <daorob@gmail.com>
2020-02-27 05:52:48 -0600

    Refactor labelText and buttonText.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    const buttonText = 'Click me';
-    
+    const buttonText = { text: 'Click me '};
+    const labelText = 'Enter name:';
+
     return (
         <div>
             <label className="label" for="name">
-                Enter name:
+                {labelText}
             </label>
             <input id="name" type="text" />
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {buttonText}
+                {buttonText.text}
             </button>
         </div>
     );
